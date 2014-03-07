344a5e688c0845ab14e126c75606eed580e3c481
EvilBeaver <ovsiankin.aa@gmail.com>
2014-03-07 17:59:06 +0400

    root style selector

--- src/PhotoTagManager/ViewModel/ImageStreamViewModel.cs
+++ src/PhotoTagManager/ViewModel/ImageStreamViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Controls;
 using Tagger.Engine;
 
 namespace PhotoTagManager.ViewModel
@@ -11,7 +12,6 @@ namespace PhotoTagManager.ViewModel
     {
         IImageStream _model;
         ObservableCollection<ImageInfo> _images;
-        //private bool _isSelected;
 
         public ImageStreamViewModel(IImageStream model)
         {
@@ -53,19 +53,6 @@ namespace PhotoTagManager.ViewModel
             }
         }
 
-        //public bool IsSelected 
-        //{
-        //    get
-        //    {
-        //        return _isSelected;
-        //    }
-        //    set
-        //    {
-        //        _isSelected = value;
-        //        OnPropertyChanged("IsSelected");
-        //    }
-        //}
-
     }
 
     class DummyImageStreamViewModel : ImageStreamViewModel
@@ -84,6 +71,26 @@ namespace PhotoTagManager.ViewModel
         protected override string GetHeader()
         {
             return "loading...";
+        }
+    }
+
+    class CategoryRootStyleSelector : StyleSelector
+    {
+        public override System.Windows.Style SelectStyle(object item, System.Windows.DependencyObject container)
+        {
+            var element = container as TreeViewItem;
+
+            if (element != null && item is ImageCategoryViewModel)
+            {
+                return element.FindResource("StreamHeaderStyle") as System.Windows.Style;
+            }
+
+            if (element != null && item is ImageStreamViewModel)
+            {
+                return element.FindResource("StandartStyle") as System.Windows.Style;
+            }
+
+            return null;
         }
     }
 }
